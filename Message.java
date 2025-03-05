47871960bae8e66f6588054c9415cb8bc2640ede
Moein <moein9805@gmail.com>
2025-03-06 00:14:39 +0600

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,33 @@
+public class Message {
+    public static void main(String[] args) {
+        // Write a message that call "Moein is cool" 5 times
+        message();
+    }
+
+    static  void message(){
+
+        System.out.println("Moein is cool");
+        message1();
+    }
+    static  void message1(){
+
+        System.out.println("Moein is cool");
+        message2();
+    }
+    static  void message2(){
+
+        System.out.println("Moein is cool");
+        message3();
+    }
+    static  void message3(){
+
+        System.out.println("Moein is cool");
+        message4();
+    }
+    static  void message4(){
+
+        System.out.println("Moein is cool");
+
+    }
+
+}
